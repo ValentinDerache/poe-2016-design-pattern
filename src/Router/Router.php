834efd927d4386abf8ea6454e9109b21224ae15f
formation <formation@pm-00045.local>
2016-07-29 10:38:57 +0200

    init

---
+++ src/Router/Router.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: formation
+ * Date: 29/07/2016
+ * Time: 10:29
+ */
+
+namespace DesignPatterns\Router;
+
+
+use Interop\Container\ContainerInterface;
+
+class Router
+{
+
+    private $config = array();
+
+    /**
+     * Router constructor.
+     * @param array $config
+     */
+    public function __construct(array $config, ContainerInterface $container)
+    {
+        $this->config = $config;
+    }
+
+
+}
